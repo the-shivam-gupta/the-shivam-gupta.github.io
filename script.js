6c2d268fd30de98174852f34a9fa3a57a4b358dc
shivam02022002 <shivamgupta02022002@gmail.com>
2023-09-12 21:36:04 +0530

    Updated the navbar attributes and styling and also updated the back to top button

--- script.js
+++ script.js
@@ -41,6 +41,6 @@ $(document).ready(function () {
 
   btn.on("click", function (e) {
     e.preventDefault();
-    $("html, body").animate({ scrollTop: 0 }, "300");
+    window.scrollTo(0, 0);
   });
 });
